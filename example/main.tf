613caf16e83870ec6e6be5c2ead8f513f264c400
engels <engels.franca@icloud.com>
2025-06-04 23:25:57 -0300

    Modulo funcional, publicado e apontando para o gitlab

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "keypair" {
-  source        = "../"
+  source        = "git::git@gitlab.com:iac5395220/modules/aws/public_key.git"
   chave_publica = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+kIkWSb0pJ2r/FU/ckt0Kl4erRdZefh2C5IXktvYMq engels.souza"
 }
 
