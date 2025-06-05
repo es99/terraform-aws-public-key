5179f352059d8c5d1705747b8f7bcfafe53c8db0
engels <engels.franca@icloud.com>
2025-06-05 00:32:02 -0300

    Adicionando 'output' informando o nome da chave.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "Retorna o id do keypair."
   value       = aws_key_pair.deployer.id
 }
+
+output "keyname" {
+  description = "O nome da chave."
+  value       = aws_key_pair.deployer.key_name
+}
